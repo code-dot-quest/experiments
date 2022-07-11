1268ac2dca73b66f45be0aedff54d5209156d50e
Tal Kol <talkol@gmail.com>
2022-07-11 21:53:27 +0100

    Added prettier

--- phaser3/src/editor.ts
+++ phaser3/src/editor.ts
@@ -1,24 +1,28 @@
-import $ from 'cash-dom';
-import * as editorJson from './editor.json';
+import $ from "cash-dom";
+import * as editorJson from "./editor.json";
 
 type OnTileSelected = (frameId: string) => void;
 
 export default class Editor {
   selectedFrameId: string = "26";
   private onTileSelectedHandler: OnTileSelected;
-  
+
   constructor() {
     const instance = this;
     $(() => {
       for (const tile of editorJson.tiles) {
-        $(`<span class="editor-tile" style="background:url(${editorJson.tilesImage.src}); background-position:-${Math.ceil(tile.frame.x/2)+1}px -${Math.ceil(tile.frame.y/2)+1}px; background-size:${editorJson.tilesImage.w/2}px ${editorJson.tilesImage.h/2}px;" x-frame="${tile.frameId}" />`).appendTo('#editor-tiles-parent');
+        $(
+          `<span class="editor-tile" style="background:url(${editorJson.tilesImage.src}); background-position:-${Math.ceil(tile.frame.x / 2) + 1}px -${
+            Math.ceil(tile.frame.y / 2) + 1
+          }px; background-size:${editorJson.tilesImage.w / 2}px ${editorJson.tilesImage.h / 2}px;" x-frame="${tile.frameId}" />`
+        ).appendTo("#editor-tiles-parent");
       }
       $("span.editor-tile").on("click", (event) => {
         $("span.editor-tile.selected").removeClass("selected");
         const frameId = $(event.currentTarget).addClass("selected").attr("x-frame");
         instance.selectedFrameId = frameId;
         if (this.onTileSelectedHandler) this.onTileSelectedHandler(frameId);
-      }); 
+      });
     });
   }
 
--- phaser3/src/game.ts
+++ phaser3/src/game.ts
@@ -1,5 +1,5 @@
-import 'phaser';
-import Editor from './editor';
+import "phaser";
+import Editor from "./editor";
 
 const TILE_SIZE = 128;
 const editor = new Editor();
@@ -11,33 +11,33 @@ export default class Demo extends Phaser.Scene {
   stamp: Phaser.GameObjects.Sprite;
 
   constructor() {
-    super('demo');
+    super("demo");
   }
 
   preload() {
-    this.load.aseprite('tiles', 'assets/tiles.png', 'assets/tiles.json');
-    this.load.aseprite('knight', 'assets/knight.png', 'assets/knight.json');
+    this.load.aseprite("tiles", "assets/tiles.png", "assets/tiles.json");
+    this.load.aseprite("knight", "assets/knight.png", "assets/knight.json");
   }
 
   create() {
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.anims.createFromAseprite('knight');
+    this.anims.createFromAseprite("knight");
 
     this.map = [];
-    for (let y=0; y<10; y++) {
+    for (let y = 0; y < 10; y++) {
       this.map[y] = [];
-      for (let x=0; x<10; x++) {
-        this.map[y][x] = this.add.sprite(x*TILE_SIZE, y*TILE_SIZE, 'tiles', 26).setOrigin(0, 0);
+      for (let x = 0; x < 10; x++) {
+        this.map[y][x] = this.add.sprite(x * TILE_SIZE, y * TILE_SIZE, "tiles", 26).setOrigin(0, 0);
       }
     }
 
-    this.stamp = this.add.sprite(0, 0, 'tiles', editor.selectedFrameId).setOrigin(0, 0).setAlpha(0.8);
+    this.stamp = this.add.sprite(0, 0, "tiles", editor.selectedFrameId).setOrigin(0, 0).setAlpha(0.8);
 
     //this.add.grid(0, 0, 1280, 1280, 128, 128).setOrigin(0, 0).setOutlineStyle(0x101010, 0.15);
-    this.add.grid(0, 0, 1280, 1280, 128, 128).setOrigin(0, 0).setOutlineStyle(0xffffff, 0.20);
+    this.add.grid(0, 0, 1280, 1280, 128, 128).setOrigin(0, 0).setOutlineStyle(0xffffff, 0.2);
 
-    this.player = this.add.sprite(100, 100, 'knight').play({ key: 'Idle', repeat: -1 });
-    this.add.sprite(200, 200, 'knight').play({ key: 'Attack_1', repeat: -1 });
+    this.player = this.add.sprite(100, 100, "knight").play({ key: "Idle", repeat: -1 });
+    this.add.sprite(200, 200, "knight").play({ key: "Attack_1", repeat: -1 });
 
     editor.onTileSelected((frameId) => {
       this.stamp.setFrame(frameId);
@@ -48,7 +48,7 @@ export default class Demo extends Phaser.Scene {
     const worldPoint = this.input.activePointer.positionToCamera(this.cameras.main) as Phaser.Math.Vector2;
     const pointerX = Math.floor(worldPoint.x / TILE_SIZE);
     const pointerY = Math.floor(worldPoint.y / TILE_SIZE);
-    this.stamp.setPosition(pointerX*TILE_SIZE, pointerY*TILE_SIZE);
+    this.stamp.setPosition(pointerX * TILE_SIZE, pointerY * TILE_SIZE);
     this.stamp.setVisible(time - this.input.activePointer.time < 2000);
     if (this.input.manager.activePointer.isDown) {
       this.map[pointerY][pointerX]?.setFrame(editor.selectedFrameId);
@@ -58,34 +58,30 @@ export default class Demo extends Phaser.Scene {
       this.player.play({ key: "Run" }, true);
       this.player.setX(this.player.x - 2);
       this.player.flipX = true;
-    }
-    else if (this.cursors.right.isDown) {
+    } else if (this.cursors.right.isDown) {
       this.player.play({ key: "Run" }, true);
       this.player.setX(this.player.x + 2);
       this.player.flipX = false;
-    }
-    else if (this.cursors.up.isDown) {
+    } else if (this.cursors.up.isDown) {
       this.player.play({ key: "Run" }, true);
       this.player.setY(this.player.y - 2);
-    }
-    else if (this.cursors.down.isDown) {
+    } else if (this.cursors.down.isDown) {
       this.player.play({ key: "Run" }, true);
       this.player.setY(this.player.y + 2);
-    }
-    else {
-      this.player.play({ key: 'Idle', repeat: -1 }, true);
+    } else {
+      this.player.play({ key: "Idle", repeat: -1 }, true);
     }
   }
 }
 
 const config = {
   type: Phaser.AUTO,
-  backgroundColor: '#125555',
+  backgroundColor: "#125555",
   width: 1280,
   height: 1280,
   scene: Demo,
   zoom: 0.5,
-  parent: 'canvas-parent'
+  parent: "canvas-parent",
 };
 
 const game = new Phaser.Game(config);
